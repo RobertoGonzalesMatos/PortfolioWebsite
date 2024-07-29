968149eda3601608ffd21ce1cba5913020bd2779
RobertoGonzalesMatos <roberto_gonzales_matos@brown.edu>
2024-07-28 23:13:52 -0700

    Feat: avoid error

--- src/components/AnimatedRouts.tsx
+++ src/components/AnimatedRouts.tsx
@@ -31,33 +31,26 @@
 // }
 
 // export default AnimatedRoutes;
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { AnimatePresence } from "framer-motion";
 import { Routes, Route, useLocation } from "react-router-dom";
 import HomePage from "./HomePage/Home";
-import CodePage from "./Code/Code";
+
+let lastLocation: string = "/";
 
 function AnimatedRoutes() {
   const location = useLocation();
   useEffect(() => {
-    const hasReloaded = sessionStorage.getItem("hasReloaded");
-    const lastLocation = sessionStorage.getItem("lastLocation");
-
-    if (lastLocation !== location.pathname && hasReloaded === "false") {
-      sessionStorage.setItem("hasReloaded", "true");
-      sessionStorage.setItem("lastLocation", location.pathname);
+    if (!(lastLocation === "/") && location.pathname === "/") {
       window.location.reload();
-    } else {
-      sessionStorage.setItem("hasReloaded", "false");
-      sessionStorage.setItem("lastLocation", location.pathname);
+      lastLocation = "/";
     }
+    lastLocation = location.pathname;
   }, [location]);
-
   return (
     <AnimatePresence mode="wait">
       <Routes location={location} key={location.key}>
         <Route path="/" element={<HomePage key="home" />} />
-        <Route path="/Code" element={<CodePage key="code" />} />
       </Routes>
     </AnimatePresence>
   );
