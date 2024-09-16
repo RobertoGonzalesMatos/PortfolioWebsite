2947a0bfb26184f11d6d467cad58438ac8c91bf2
RobertoGonzalesMatos <roberto_gonzales_matos@brown.edu>
2024-09-16 00:01:26 -0400

    Feat: code

--- src/components/Code/Code.tsx
+++ src/components/Code/Code.tsx
@@ -9,6 +9,47 @@ import PageWrapper from "../PageWrapper";
 export interface ICodeProps {}
 
 const CodePage: React.FunctionComponent<ICodeProps> = (props) => {
+  const trackRef = useRef<HTMLDivElement | null>(null);
+  const [mouseDownAt, setMouseDownAt] = useState<number>(0);
+  const [prevPercentage, setPrevPercentage] = useState<number>(0);
+  const [currentPercentage, setCurrentPercentage] = useState<number>(0);
+
+  const handleOnDown = (clientX: number) => {
+    setMouseDownAt(clientX);
+  };
+
+  const handleOnUp = () => {
+    setMouseDownAt(0);
+    setPrevPercentage(currentPercentage);
+  };
+
+  const handleOnMove = (clientX: number) => {
+    if (mouseDownAt === 0) return;
+
+    const track = trackRef.current;
+    if (!track) return;
+
+    const mouseDelta = mouseDownAt - clientX;
+    const maxDelta = window.innerWidth / 2;
+
+    const percentage = (mouseDelta / maxDelta) * -100;
+    const nextPercentageUnconstrained = prevPercentage + percentage;
+    const nextPercentage = Math.max(
+      Math.min(nextPercentageUnconstrained, 0),
+      -100
+    );
+
+    setCurrentPercentage(nextPercentage);
+
+    track.style.transform = `translate(${nextPercentage}%, -50%)`;
+
+    for (const image of track.getElementsByClassName(
+      "image"
+    ) as HTMLCollectionOf<HTMLImageElement>) {
+      image.style.objectPosition = `${100 + nextPercentage}% center`;
+    }
+  };
+
   return (
     <div>
       <Header headerColor="dark" dim={false} />
@@ -22,7 +63,7 @@ const CodePage: React.FunctionComponent<ICodeProps> = (props) => {
           >
             <div
               style={{
-                background: "white",
+                background: "black",
                 width: "100%",
                 height: "100vh",
                 display: "flex",
@@ -30,7 +71,50 @@ const CodePage: React.FunctionComponent<ICodeProps> = (props) => {
                 alignItems: "center",
               }}
             >
-              <img className="WIP" src="/PortfolioWebsite/WIP.webp" alt="WIP" />
+              <div
+                id="image-track"
+                ref={trackRef}
+                style={{ transform: `translate(${currentPercentage}%, -50%)` }}
+                onMouseDown={(e) => handleOnDown(e.clientX)}
+                onTouchStart={(e) => handleOnDown(e.touches[0].clientX)}
+                onMouseUp={handleOnUp}
+                onTouchEnd={handleOnUp}
+                onMouseMove={(e) => handleOnMove(e.clientX)}
+                onTouchMove={(e) => handleOnMove(e.touches[0].clientX)}
+                data-mouse-down-at={mouseDownAt}
+                data-prev-percentage={prevPercentage}
+              >
+                <img
+                  className="image"
+                  src="/PortfolioWebsite/3dgame.webp"
+                  draggable="false"
+                  alt="WIP"
+                />
+                <img
+                  className="image"
+                  src="/PortfolioWebsite/meiklejohn.webp"
+                  draggable="false"
+                  alt="WIP"
+                />
+                <img
+                  className="image"
+                  src="/PortfolioWebsite/room3D.webp"
+                  draggable="false"
+                  alt="WIP"
+                />
+                <img
+                  className="image"
+                  src="/PortfolioWebsite/HYT.webp"
+                  draggable="false"
+                  alt="WIP"
+                />
+                <img
+                  className="image"
+                  src="/PortfolioWebsite/WIP.webp"
+                  draggable="false"
+                  alt="WIP"
+                />
+              </div>
             </div>
           </div>
         }
